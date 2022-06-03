e2015bda4236e129a2065310bb258a43113eca3a
Mac PRO <macpro@Macs-MacBook-Pro.local>
2022-06-03 18:16:49 +0700

    add services

---
+++ services/userService.js
@@ -0,0 +1,22 @@
+const db = require('./db');
+const helper = require('../helper');
+const config = require('../config');
+
+async function getUsers(page = 1){
+  const offset = helper.getOffset(page, config.listPerPage);
+  const rows = await db.query(
+    `SELECT iduser, fullname, email
+    FROM user LIMIT ${offset},${config.listPerPage}`
+  );
+  const data = helper.emptyOrRows(rows);
+  const meta = {page};
+
+  return {
+    data,
+    meta
+  }
+}
+
+module.exports = {
+    getUsers
+}
