8ff509f96c3c4d7eb2bd28aac72158a1e49986ec
Mac PRO <macpro@Macs-MacBook-Pro.local>
2022-06-26 22:05:42 +0700

    add validation role login for admin endpoint

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -2,17 +2,30 @@ const express = require('express');
 const router = express.Router();
 const mnguserService = require('../services/manageUserService');
 
-/* GET User. */
+
 router.get('/admin/listOfUsers', async function(req, res, next) {
   try {
     const token =  req.header('token');
     console.log('header token:'+token);
-    res.json(await mnguserService.getUsers(req.query.page));
+    res.json(await mnguserService.getUsers(req.query.page,token));
 
   } catch (err) {
     console.error(`Error while getting data `, err.message);
     next(err);
   }
 });
+
+
+router.get('/admin/resetUserPassword', async function(req, res, next) {
+    try {
+      const token =  req.header('token');
+      console.log('header token:'+token);
+      res.json(await mnguserService.getUsers(req.query.page));
+  
+    } catch (err) {
+      console.error(`Error while getting data `, err.message);
+      next(err);
+    }
+  });
 
 module.exports = router;
--- services/manageUserService.js
+++ services/manageUserService.js
@@ -10,21 +10,104 @@ join role r
 on
 ur.idrole  = r.idrole `;
 
-async function getUsers(page = 1) {
-  const offset = helper.getOffset(page, config.listPerPage);
-  const rows = await db.query(
-    queryUser+` LIMIT ${offset},${config.listPerPage}`
-  );
-  const data = helper.emptyOrRows(rows);
-  const meta = { page };
+async function getUsers(page = 1,token) {
 
-  return {
-    data,
-    meta
-  }
+
+  return db.query(
+    `CALL checkToken(?,@out_value);CALL sp_ReadReturnValue();`, [token]
+  ).then((result) => {
+    var jsonResponse = { "message": "", "data": {} };
+    const responseToken = result[1][0].ret_value;
+    if (responseToken == token) {
+      jsonResponse.data = {};
+      jsonResponse.message = '';
+    } else {
+      jsonResponse.message = responseToken;
+    }
+    return jsonResponse;
+
+  }).catch((err) => {
+    !error.logged && console.error(err);
+    res.status(500).send();
+  })
+    .then(response => {
+
+      return db.query('SELECT ll.iduser,m.role_name,r.idrole FROM login_log ll JOIN user_role r ON r.iduser=ll.iduser JOIN role m ON r.idrole=m.idrole WHERE ll.token=?', [token])
+        .then(resultLoginLog => {
+          const roleUser = resultLoginLog[0] != null ? resultLoginLog[0] : 0;
+          return roleUser;
+        }).then(roleUser => {
+
+          if (roleUser.role_name == 'administrator') {
+            const offset = helper.getOffset(page, config.listPerPage);
+            return db.query(
+              queryUser + ` LIMIT ${offset},${config.listPerPage}`
+            ).then(rows => {
+              const data = helper.emptyOrRows(rows);
+              const meta = { page };
+              return {
+                message:'',
+                data,
+                meta
+              }
+            });
+
+
+          } else {
+            return {
+              message: 'you dont have authorize',
+              data:[]
+            }
+          }
+
+
+        });
+
+    });
+
+
+}
+
+async function resetPassword(token, new_pwd) {
+
+  return db.query(
+    `CALL checkToken(?,@out_value);CALL sp_ReadReturnValue();`, [token]
+  ).then((result) => {
+    var jsonResponse = { "message": "", "data": {} };
+    const responseToken = result[1][0].ret_value;
+    if (responseToken == token) {
+      jsonResponse.data = {};
+      jsonResponse.message = '';
+    } else {
+      jsonResponse.message = responseToken;
+    }
+    return jsonResponse;
+
+  }).catch((err) => {
+    !error.logged && console.error(err);
+    res.status(500).send();
+  })
+    .then(response => {
+
+      return db.query('SELECT iduser FROM login_log WHERE token=?', [token])
+        .then(resultLoginLog => {
+          const iduser = resultLoginLog[0] != null ? resultLoginLog[0].iduser : 0;
+          return iduser;
+        }).then(iduser => {
+          return db.query('UPDATE user SET pwd=MD5(?) WHERE iduser=?', [new_pwd, iduser])
+            .then(user => {
+
+              response.data = user;
+              return response;
+            });
+        });
+
+    });
+
 }
 
 
 module.exports = {
-  getUsers
+  getUsers,
+  resetPassword
 }
