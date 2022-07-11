9755beee0549a603417927c79361adcd3544aa55
David Chandra <davidchandra509@gmail.com>
2022-07-11 10:25:28 +0700

    mencoba function login dengan node_module

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const config = {
-    db: {
-      /* don't expose password or any sensitive info, done only for demo */
-      host: "ticketing_db_1",
-      //host:"localhost",
-      user: "root",
-      password: "admin",
-      database: "ticketing",
-      multipleStatements:true,
-      debug: true,
-      rowsAsArray: false
-    },
-    listPerPage: 10,
-    jumlahStudio: 10,
-    jumlahKursi: 25
-  };
-  module.exports = config;
+  db: {
+    /* don't expose password or any sensitive info, done only for demo */
+    //host: "ticketing_db_1",
+    host: "localhost",
+    user: "root",
+    password: "admin",
+    database: "ticketing",
+    multipleStatements: true,
+    debug: true,
+    rowsAsArray: false
+  },
+  listPerPage: 10,
+  jumlahStudio: 10,
+  jumlahKursi: 25
+};
+module.exports = config;
